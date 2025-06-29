aa81c12132722224c0177f7df7b0d443c59ebcc5
Lavanya Bhadani <lavi.bhadani@gmail.com>
2025-06-29 18:08:13 +0530

    Initial file

---
+++ dist/script.js
@@ -0,0 +1,24 @@
+document.getElementById('theme-toggle').addEventListener('click', () => {
+    const themeLink = document.getElementById('theme-style');
+    themeLink.href = themeLink.href.includes('light.css') ? './dark.css' : './light.css';
+});
+
+const hamburger = document.getElementById("hamburger");
+const line1 = document.getElementById("line1");
+const line2 = document.getElementById("line2");
+const line3 = document.getElementById("line3");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("hidden");
+
+  // Toggle cross animation
+  line1.classList.toggle("rotate-45");
+  line1.classList.toggle("translate-y-[9px]"); // push down
+
+  line2.classList.toggle("opacity-0");
+
+  line3.classList.toggle("-rotate-45");
+  line3.classList.toggle("-translate-y-[9px]"); // pull up
+});
+
