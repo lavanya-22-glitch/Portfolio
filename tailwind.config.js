7a11388678c1bfd68c70af4ec7b3a0de650672d8
Lavanya Bhadani <lavi.bhadani@gmail.com>
2025-06-29 17:29:26 +0530

    Initial file

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./dist/*.html",
+    "./dist/*.js", 
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        bree: ['"Bree Serif"', 'serif'],
+      },
+      colors: {
+        blush: {
+          light: '#FFF7F4',
+          beige: '#EAD6CD',
+          rose: '#D36F6F',
+          cocoa: '#3C2A2A',
+          card: '#F8E3DC',
+          accent: '#C97B84',
+          hover: '#D9B4A8',
+        },
+        night: {
+          bg: '#1C1B1E',
+          text: '#FCE8E4',
+          accent: '#F6B7B7',
+          box: '#2F2C2D',
+          nav: '#272324',
+          hover: '#5A4A4A',
+        }
+      }
+    },
+  },
+  plugins: [],
+  darkMode: "class",
+}
